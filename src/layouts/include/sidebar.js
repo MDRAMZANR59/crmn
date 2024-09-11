ecbf39ab9245fe663ef6b4941efd41f49dcba49e
Md Akash <you@example.com>
2024-09-12 00:45:25 +0600

    Edit Link Of Sidebar

--- src/layouts/include/sidebar.js
+++ src/layouts/include/sidebar.js
@@ -57,33 +57,32 @@ function Sidebar() {
                 <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 {/* Add icons to the links using the .nav-icon class
                     with font-awesome or any other icon font library */}
-                <li className="nav-item menu-open">
-                    <a href="#" className="nav-link active">
-                    <i className="nav-icon fas fa-tachometer-alt"></i>
-                    <p>
-                        Dashboard
-                        <i className="right fas fa-angle-left"></i>
-                    </p>
+                <li className={`nav-item menu-open`} onClick={activeMenu}>
+                    <a href="#" className="nav-link active" >
+                        <i className="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard<i className="right fas fa-angle-left"></i></p>
                     </a>
-                    <ul className="nav nav-treeview">
-                    <li className="nav-item">
-                        <a href="./index.html" className="nav-link active">
-                        <i className="far fa-circle nav-icon"></i>
-                        <p>Dashboard v1</p>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="./index2.html" className="nav-link">
-                        <i className="far fa-circle nav-icon"></i>
-                        <p>Dashboard v2</p>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="./index3.html" className="nav-link">
-                        <i className="far fa-circle nav-icon"></i>
-                        <p>Dashboard v3</p>
-                        </a>
-                    </li>
+                    <ul className={`nav nav-treeview ${isLinkActive("/Dashboard1")}`}>
+                        <li className="nav-item">
+                            
+                               <Link to={"/Dashboard1"}>
+                                    <i className="far fa-circle nav-icon"></i>
+                                    <p>Dashboard v1</p>
+                               </Link>
+                           
+                        </li>
+                        <li className="nav-item">
+                            <a href="./index.html" className="nav-link">
+                                <i className="far fa-circle nav-icon"></i>
+                                <p>Dashboard v2</p>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="./index3.html" className="nav-link">
+                                <i className="far fa-circle nav-icon"></i>
+                                <p>Dashboard v3</p>
+                            </a>
+                        </li>
                     </ul>
                 </li>
                 <li className="nav-item">
