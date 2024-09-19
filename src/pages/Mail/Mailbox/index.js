6ec5a2737c29b3f684d6972a4a38b78b5afbf75b
Nurkhankazi <WDPF@nh.ctg>
2024-09-19 14:20:03 +0600

    edit site bar

--- src/pages/Mail/Mailbox/index.js
+++ src/pages/Mail/Mailbox/index.js
@@ -18,8 +18,8 @@ function MailBox() {
             <div className="content-wrapper">
                 <div className="content-header">
                     <div className="container-fluid">
-                        <div className="row mb-2">
-                            <div className="col-sm-6">
+                        <div className="row">
+                            <div className="col-sm-2">
                                 <h1 className="m-0">Mail Box</h1>
                             </div>
                         </div>
@@ -28,36 +28,43 @@ function MailBox() {
                 <section>
                     <div className='container'>
                         <div className='row'>
-                            <div className='col-sm-4 bg-secondary text- h5'>
-                                <ul >
-                                    <li className='list-unstyle text-secondary' ><a href="#" className='bg-primary text'>Compose</a></li><hr/>
-                                    <li className='list-unstyle text-secondary' ><a href="#" >Inbox</a></li><hr/>
-                                    <li className='list-unstyle text-secondary' ><a href="#" >Sent</a></li><hr/>
-                                    <li className='list-unstyle text-secondary' ><a href="#" >Draft</a></li>
+                            <div className=' ms-1 border rounded col-sm-2 bg-secondary text- h5'>
+                                <ul className='ps-0 text-center'>
+                                    <li className='list-unstyled text-secondary rounded bg-primary ' ><a href="#" className='text-white text-decoration-none'>Compose</a></li><hr/>
+                                    <li className='list-unstyled text-secondary rounded' ><a href="#" className='text-white text-decoration-none' >Inbox</a></li><hr/>
+                                    <li className='list-unstyled text-secondary rounded' ><a href="#" className='text-white text-decoration-none' >Sent</a></li><hr/>
+                                    <li className='list-unstyled text-secondary rounded' ><a href="#" className='text-white text-decoration-none' >Draft</a></li>
                                 </ul>
-
                             </div>
-                        </div>
-
-                    </div>
-                </section>
-                {/* <section className="content">
-                    <div className="container-fluid">
-                        <div className="card card-default">
+                            <div className="card col-sm-9 ms-1 card-default">
                             <div className="card-header">
                                 <h3 className="card-title">Inbox/Sen/Draft/...</h3>
-                                
+                            </div>
+                            <div>
+                                Hello
                             </div>
                             <div className="card-body">
                                 <div className="container mt-5">
                                     <form onSubmit={handleSubmit}>
+                                        <ul>
+                                            <li className='list-unstyled' >
+                                                <div className=' d-inline col-md-1'><input type="checkbox"/></div>
+                                                <div className=' d-inline col-md-1'>Star</div>
+                                                <div className=' d-inline col-md-3'>Subject</div>
+                                                <div className=' d-inlinecol-md-6'>Hello World Hello World Hello World</div>
+                                                <div className=' d-inline col-md-1'></div>
+                                            </li>
+                                        </ul>
                                         
                                     </form>
                                 </div>
                             </div>
                         </div>
+                        </div>
+
                     </div>
-                </section> */}
+                </section>
+               
             </div>
         </AdminLayout>
     );
