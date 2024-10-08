53308c580bb5b7771a34aa284465ec5230340dea
Md Akash <you@example.com>
2024-10-08 23:33:00 +0600

    Edit User Profile Page

--- src/pages/User/Profile/index.js
+++ src/pages/User/Profile/index.js
@@ -1415,6 +1415,190 @@ function UserProfile() {
                         {/* /.tab-content */}
                       </div>{/* /.card-body */}
                     </div>
+                    <div className="card table-responsive ">
+                      <div className="card-header p-2">
+                        <ul className=" ps-0 nav-pills">
+                          <li className="nav-item list-unstyled"><a className="nav-link active" href="#activity" data-toggle="tab">Complain/Suggestion</a></li>
+                        </ul>
+                      </div>
+                      <div className="card-body p-0">
+                        <div className="tab-content">
+                          <div className="active tab-pane" id="activity">
+                            <section className="content">
+                                <div className="card">
+                                  <div className="card-body p-0">
+                                  <form onSubmit={handleSubmit}>
+                                    <div className="mb-12 col-md-12">
+                                        <label className="me-2" htmlFor="complain"><input type="radio" name="complain" id="complain" />  Complain</label>
+                                        <label htmlFor="suggestion"><input type="radio" name="complain" id="suggestion" />Suggestion</label>
+                                        <textarea
+                                            required
+                                            placeholder="Write Your Complain Or Suggestion"
+                                            className={`form-control ${errors.complainSuggestion ? 'is-invalid' : ''}`}
+                                            id="complainSuggestion"
+                                            name="complainSuggestion"
+                                            value={formData.complainSuggestion}
+                                            onChange={handleChange}
+                                        ></textarea>
+                                        {errors.complainSuggestion && <div className="invalid-feedback">{errors.complainSuggestion}</div>}
+                                        <button type="submit" className="btn btn-primary mt-3">Sent</button>
+                                    </div>
+                                    </form>
+                                  </div>
+                                {/* /.card-body */}
+                                </div>
+                            </section>
+                          </div>
+                          {/* /.tab-pane */}
+                          <div className="tab-pane" id="timeline">
+                            {/* The timeline */}
+                            <div className="timeline timeline-inverse">
+                              {/* timeline time label */}
+                              <div className="time-label">
+                                <span className="bg-danger">
+                                  10 Feb. 2014
+                                </span>
+                              </div>
+                              {/* /.timeline-label */}
+                              {/* timeline item */}
+                              <div>
+                                <i className="fas fa-envelope bg-primary"></i>
+
+                                <div className="timeline-item">
+                                  <span className="time"><i className="far fa-clock"></i> 12:05</span>
+
+                                  <h3 className="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
+
+                                  <div className="timeline-body">
+                                    Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
+                                    weebly ning heekya handango imeem plugg dopplr jibjab, movity
+                                    jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
+                                    quora plaxo ideeli hulu weebly balihoo...
+                                  </div>
+                                  <div className="timeline-footer">
+                                    <a href="#" className="btn btn-primary btn-sm">Read more</a>
+                                    <a href="#" className="btn btn-danger btn-sm">Delete</a>
+                                  </div>
+                                </div>
+                              </div>
+                              {/* END timeline item */}
+                              {/* timeline item */}
+                              <div>
+                                <i className="fas fa-user bg-info"></i>
+
+                                <div className="timeline-item">
+                                  <span className="time"><i className="far fa-clock"></i> 5 mins ago</span>
+
+                                  <h3 className="timeline-header border-0"><a href="#">Sarah Young</a> accepted your friend request
+                                  </h3>
+                                </div>
+                              </div>
+                              {/* END timeline item */}
+                              {/* timeline item */}
+                              <div>
+                                <i className="fas fa-comments bg-warning"></i>
+
+                                <div className="timeline-item">
+                                  <span className="time"><i className="far fa-clock"></i> 27 mins ago</span>
+
+                                  <h3 className="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
+
+                                  <div className="timeline-body">
+                                    Take me to your leader!
+                                    Switzerland is small and neutral!
+                                    We are more like Germany, ambitious and misunderstood!
+                                  </div>
+                                  <div className="timeline-footer">
+                                    <a href="#" className="btn btn-warning btn-flat btn-sm">View comment</a>
+                                  </div>
+                                </div>
+                              </div>
+                              {/* END timeline item */}
+                              {/* timeline time label */}
+                              <div className="time-label">
+                                <span className="bg-success">
+                                  3 Jan. 2014
+                                </span>
+                              </div>
+                              {/* /.timeline-label */}
+                              {/* timeline item */}
+                              <div>
+                                <i className="fas fa-camera bg-purple"></i>
+
+                                <div className="timeline-item">
+                                  <span className="time"><i className="far fa-clock"></i> 2 days ago</span>
+
+                                  <h3 className="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+
+                                  <div className="timeline-body">
+                                    <img src="https://placehold.it/150x100" alt="..."/>
+                                    <img src="https://placehold.it/150x100" alt="..."/>
+                                    <img src="https://placehold.it/150x100" alt="..."/>
+                                    <img src="https://placehold.it/150x100" alt="..."/>
+                                  </div>
+                                </div>
+                              </div>
+                              {/* END timeline item */}
+                              <div>
+                                <i className="far fa-clock bg-gray"></i>
+                              </div>
+                            </div>
+                          </div>
+                          {/* /.tab-pane */}
+
+                          <div className="tab-pane" id="settings">
+                            <form className="form-horizontal">
+                              <div className="form-group row">
+                                <label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
+                                <div className="col-sm-10">
+                                  <input type="email" className="form-control" id="inputName" placeholder="Name"/>
+                                </div>
+                              </div>
+                              <div className="form-group row">
+                                <label htmlFor="inputEmail" className="col-sm-2 col-form-label">Email</label>
+                                <div className="col-sm-10">
+                                  <input type="email" className="form-control" id="inputEmail" placeholder="Email"/>
+                                </div>
+                              </div>
+                              <div className="form-group row">
+                                <label htmlFor="inputName2" className="col-sm-2 col-form-label">Name</label>
+                                <div className="col-sm-10">
+                                  <input type="text" className="form-control" id="inputName2" placeholder="Name"/>
+                                </div>
+                              </div>
+                              <div className="form-group row">
+                                <label htmlFor="inputExperience" className="col-sm-2 col-form-label">Experience</label>
+                                <div className="col-sm-10">
+                                  <textarea className="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                                </div>
+                              </div>
+                              <div className="form-group row">
+                                <label htmlFor="inputSkills" className="col-sm-2 col-form-label">Skills</label>
+                                <div className="col-sm-10">
+                                  <input type="text" className="form-control" id="inputSkills" placeholder="Skills"/>
+                                </div>
+                              </div>
+                              <div className="form-group row">
+                                <div className="offset-sm-2 col-sm-10">
+                                  <div className="checkbox">
+                                    <label>
+                                      <input type="checkbox"/> I agree to the <a href="#">terms and conditions</a>
+                                    </label>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="form-group row">
+                                <div className="offset-sm-2 col-sm-10">
+                                  <button type="submit" className="btn btn-danger">Submit</button>
+                                </div>
+                              </div>
+                            </form>
+                          </div>
+                          {/* /.tab-pane */}
+                        </div>
+                        {/* /.tab-content */}
+                      </div>{/* /.card-body */}
+                    </div>
                     {/* /.card */}
                   </div>
                   {/* /.col */}
