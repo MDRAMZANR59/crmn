7b075da2d2933a885930bc63ab0fc1802c13d788
Md Akash <you@example.com>
2024-11-22 23:15:21 +0600

    Add Pi Chart

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,67 +1,41 @@
 import React from 'react'
 import AdminLayout from '../../layouts/AdminLayout'
+import ApexCharts from 'apexcharts'
 
 function Dashboard() {
   return (
     <AdminLayout>
-      {/* <div className="container">
-        <div className='Container-fluid'>
-          <div className='col-md-9'>
-            <div me-2>
-              <img src=".." />
-            </div>
-            <div className='card'>
-              <div className='card-title bg-dard'>
-                Appoinment History
+      <div className="content-wrapper">
+        <div className="content-header">
+          <div className="container-fluid">
+            <div className="row mb-2">
+              <div className="col-sm-6">
+                <h1 className="m-0">Dashboard</h1>
               </div>
-              <div className='card-body bg-info'>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Doctor</th>
-                      <th>Patient</th>
-                      <th>Reserved</th>
-                      <th>Schedule</th>
-                      <th>Status</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Md Ramzan</td>
-                      <td>Md Mukut</td>
-                      <td>
-                        <p className='border border-pill'>Serial:20</p>
-                        <p className='border border-pill'><i class="fas fa-file-alt"></i></p>
-                      </td>
-                      <td>
-                        
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+              <div className="col-sm-6">
+                <ol className="breadcrumb float-sm-right">
+                  <li className="breadcrumb-item"><a href="#">Home</a></li>
+                  <li className="breadcrumb-item active">Dashboard</li>
+                </ol>
               </div>
-
             </div>
           </div>
         </div>
-      </div> */}
-<div className="content-wrapper">
-  <div className="content-header">
-    <div className="container-fluid">
-      <div className="row mb-2">
-        <div className="col-sm-6">
-          <h1 className="m-0">Dashboard</h1>
-        </div>
-        <div className="col-sm-6">
-          <ol className="breadcrumb float-sm-right">
-            <li className="breadcrumb-item"><a href="#">Home</a></li>
-            <li className="breadcrumb-item active">Dashboard</li>
-          </ol>
-        </div>
+        <section>
+          <div className='Container'>
+            <div className='row'>
+              <div className='col-md-12'>
+                <div className='col-md-6'>
+                  <ApexChart/>
+                </div>
+                <div className='col-md-6'>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    </div>
-  </div>
-</div>
     </AdminLayout>       
   )
 }
